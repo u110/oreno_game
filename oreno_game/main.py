d2641786df6a71762944f725185f8e2f6fccec5d
u110 <u110@users.noreply.github.com>
2018-12-13 22:35:32 +0900

    左から右へうけながす

---
+++ oreno_game/main.py
@@ -0,0 +1,19 @@
+import pyxel
+
+
+class App:
+    def __init__(self):
+        pyxel.init(160, 120)
+        self.x = 0
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        self.x = (self.x + 1) % pyxel.width
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.rect(self.x, 0, self.x + 7, 7, 9)
+
+
+if __name__ == "__main__":
+    App()
